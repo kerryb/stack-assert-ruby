17d749ff010eea00a1949a53fc98d510c1ccbb19
Kerry Buckley <kerryjbuckley@gmail.com>
2014-06-28 10:03:09 +0100

    Second test (errors on missing push method)

--- stack.rb
+++ stack.rb
@@ -5,3 +5,7 @@ class Stack
 end
 
 Stack.new.empty? or $stderr.puts "A new stack should be empty"
+
+stack = Stack.new
+stack.push :foo
+!stack.empty? or $stderr.puts "A stack that's been pushed to should not be empty"
