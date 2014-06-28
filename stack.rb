ec3ae0c90deb473ae58c40ef36c0daeb8820b1e4
Kerry Buckley <kerryjbuckley@gmail.com>
2014-06-28 10:17:27 +0100

    Failing test for #pop

--- stack.rb
+++ stack.rb
@@ -7,7 +7,11 @@ class Stack
     @empty = false
   end
 
+  def pop
+  end
+
   def empty?
+    @empty
   end
 end
 
@@ -20,3 +24,7 @@ assert_equal true, Stack.new.empty?, "A new stack should be empty"
 stack = Stack.new
 stack.push :foo
 assert_equal false, stack.empty?, "A stack that's been pushed to should not be empty"
+
+stack = Stack.new
+stack.push :foo
+assert_equal :foo, stack.pop, "Popping a stack after pushing an item should return the item"
