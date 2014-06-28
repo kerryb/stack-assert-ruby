0acd4973ace393bb97a314bd39ab2b4dc7c1db02
Kerry Buckley <kerryjbuckley@gmail.com>
2014-06-28 10:14:46 +0100

    Extract an assert_equal method

--- stack.rb
+++ stack.rb
@@ -8,12 +8,15 @@ class Stack
   end
 
   def empty?
-    @empty
   end
 end
 
-Stack.new.empty? or $stderr.puts "A new stack should be empty"
+def assert_equal expected, actual, message
+  expected == actual or $stderr.puts message
+end
+
+assert_equal true, Stack.new.empty?, "A new stack should be empty"
 
 stack = Stack.new
 stack.push :foo
-!stack.empty? or $stderr.puts "A stack that's been pushed to should not be empty"
+assert_equal false, stack.empty?, "A stack that's been pushed to should not be empty"
