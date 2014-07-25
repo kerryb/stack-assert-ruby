09e63d9e5c4a0f2389a70bda4b87eee7941a0989
Kerry Buckley <kerryjbuckley@gmail.com>
2014-07-25 18:44:34 +0100

    Don't stop on test failure. Almost a proper test framework

--- stack.rb
+++ stack.rb
@@ -1,69 +1,121 @@
 class Stack
-	def initialize
-		@items = []
-	end
+  def initialize
+    @items = []
+  end
 
-	def empty?
-		@items.size.zero?
-	end
+  def empty?
+    @items.size.zero?
+  end
 
-	def push *items
-		@items += items
-	end
+  def push *items
+    @items += items
+  end
 
-	def pop
-		raise StackEmptyException if empty?
-		@items.delete_at(-1)
-	end
+  def pop
+    raise StackEmptyException if empty?
+    @items.delete_at(-1)
+  end
 end
 
 class StackEmptyException < RuntimeError
 end
 
-def assert_equal expected, actual, message
-	actual == expected or fail "#{message} (expected #{expected.inspect}, got #{actual.inspect})"
+############ TEST FRAMEWORK ############
+
+@tests = []
+
+Test = Struct.new :description, :block do
+  def run
+    block.call
+    :pass
+  rescue TestFailure => e
+    $stderr.puts "FAILED: #{description} (#{e.message})"
+    :fail
+  rescue Exception => e
+    $stderr.puts "ERROR: #{description} (#{e.message})"
+    :error
+  end
 end
 
-def assert_true actual, message
-	assert_equal true, actual, message
+class TestFailure < RuntimeError
 end
 
-def assert_false actual, message
-	assert_equal false, actual, message
+def specify description, &block
+  @tests << Test.new(description, block)
 end
 
-assert_true Stack.new.empty?, "A new stack should be empty"
-
-stack = Stack.new
-stack.push 1
-assert_false stack.empty?, "A stack should not be empty after pushing"
-
-stack = Stack.new
-stack.push "I am an item."
-assert_equal "I am an item.", stack.pop, "Popping an item from the stack should return the last item added."
-
-stack = Stack.new
-stack.push "Item"
-stack.pop
-assert_true stack.empty?, "Stack should be empty after popping the last item."
-
-stack = Stack.new
-stack.push 1
-stack.push 2
-assert_equal [2,1], [stack.pop, stack.pop], "Stack should pop items in the reverse order that they were pushed."
-
-stack = Stack.new
-stack.push 1, 2
-stack.pop
-assert_false stack.empty?, "A stack should not be empty if there are still items remaining"
-
-stack = Stack.new
-begin
-	stack.pop
-	fail "An empty stack should throw an exception"
-rescue StackEmptyException
+at_exit do
+  results = @tests.map(&:run)
+  passes = results.count(:pass)
+  failures = results.count(:fail)
+  errors = results.count(:error)
+  puts "#{results.size} tests. #{passes} passed, #{failures} failed, #{errors} error#{'s' unless errors == 1}"
+  exit failures + errors
 end
 
-stack = Stack.new
-stack.push 1, 2
-assert_equal [2,1], [stack.pop, stack.pop], "Push should accept multiple items."
+def assert_equal expected, actual
+  actual == expected or fail TestFailure.new("expected #{expected.inspect}, got #{actual.inspect}")
+end
+
+def assert_true actual
+  assert_equal true, actual
+end
+
+def assert_false actual
+  assert_equal false, actual
+end
+
+def assert_raises error, &block
+  block.call
+  fail TestFailure.new("expected #{error}, but nothing was raised")
+rescue error
+end
+############ TESTS ############
+
+specify "A new stack should be empty" do
+  assert_true Stack.new.empty?
+end
+
+specify "A stack should not be empty after pushing" do
+  stack = Stack.new
+  stack.push 1
+  assert_false stack.empty?
+end
+
+specify "Popping an item from the stack should return the last item added" do
+  stack = Stack.new
+  stack.push 1
+  assert_equal 1, stack.pop
+end
+
+specify "Stack should be empty after popping the last item" do
+  stack = Stack.new
+  stack.push 1
+  stack.pop
+  assert_true stack.empty?
+end
+
+specify "Stack should pop items in the reverse order that they were pushed" do
+  stack = Stack.new
+  stack.push 1
+  stack.push 2
+  assert_equal [2,1], [stack.pop, stack.pop]
+end
+
+specify "A stack should not be empty if there are still items remaining" do
+  stack = Stack.new
+  stack.push 1, 2
+  stack.pop
+  assert_false stack.empty?
+end
+
+specify "An empty stack should throw an exception" do
+  stack = Stack.new
+  assert_raises(StackEmptyException) { stack.pop }
+end
+
+specify "Push should accept multiple items" do
+  stack = Stack.new
+  stack.push 1, 2
+  assert_equal [2,1], [stack.pop, stack.pop]
+end
