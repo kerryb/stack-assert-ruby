cceb07149e1b3951fd27a9d18130064e208c52fe
Kerry Buckley <kerryjbuckley@gmail.com>
2014-08-01 12:04:01 +0100

    Second week designery (add specify etc)

--- stack.rb
+++ stack.rb
@@ -1,3 +1,46 @@
+def assert_equal expected, actual
+  if expected != actual
+    fail
+  end
+end
+
+def assert_true actual
+  assert_equal(true, actual)
+end
+
+def assert_false actual
+  assert_equal(false, actual)
+end
+
+def assert_exception exception_class, &test_code
+  begin
+    yield test_code
+    fail
+  rescue exception_class
+  end
+end
+
+def specify message, &test_case
+  begin
+    yield test_case
+    print "."
+  rescue => e
+    print "F"
+    @messages << message
+    @messages << e.backtrace
+    @messages << "\n"
+  end
+end
+
+def expound message, &test_cases
+  @messages = []
+
+  yield test_cases
+
+  puts
+  puts @messages.join "\n"
+end
+
 class Stack
 
   class Empty < RuntimeError; end
@@ -20,38 +63,41 @@ class Stack
   end
 end
 
-def assert_equal expected, actual, message
-  if expected != actual
-    fail message
-  else
-    print "."
+expound "some stuff" do
+
+  specify "A new stack is empty" do
+    assert_true(Stack.new.empty?)
+  end
+
+  specify "A stack with something pushed to it is not empty" do
+    stack = Stack.new
+    # stack.push(1)
+    assert_false(stack.empty?)
+  end
+
+  specify "Popping from a stack with one item returns the last added item" do
+    stack = Stack.new
+    stack.push(1)
+    assert_equal(1, stack.pop)
+  end
+
+  specify "Popping an empty stack raises the empty stack exception" do
+    assert_exception Stack::Empty do
+      Stack.new.pop
+    end
+  end
+
+  specify "A stack is empty after popping the last item" do
+    stack = Stack.new
+    stack.push(1)
+    stack.pop
+    assert_true stack.empty?
+  end
+
+  specify "Items are popped LIFO" do
+    stack = Stack.new
+    stack.push(1)
+    stack.push(2)
+    assert_equal([2,1], [stack.pop, stack.pop])
   end
 end
-
-assert_equal(true, Stack.new.empty?, "A new stack is empty")
-
-stack = Stack.new
-stack.push(1)
-assert_equal(false, stack.empty?, "A stack with something pushed to it is not empty")
-
-stack = Stack.new
-stack.push(1)
-assert_equal(1, stack.pop, "Popping from a stack with one item returns the last added item")
-
-begin
-  Stack.new.pop
-  fail "Popping an empty stack raises the empty stack exception"
-rescue Stack::Empty
-end
-
-stack = Stack.new
-stack.push(1)
-stack.pop
-assert_equal(true, stack.empty?, "A stack is empty after popping the last item")
-
-stack = Stack.new
-stack.push(1)
-stack.push(2)
-assert_equal([2,1], [stack.pop, stack.pop], "Items are popped LIFO")
-
-
